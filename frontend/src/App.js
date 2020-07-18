e09cdc31c000452f90a60cbcc60ae3718a268dba
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-07-18 19:01:06 -0400

    added some extra tags to seperate from other react apps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <h2>My Name is Universal Philosopher</h2>
+      <p>This is my website</p>
     </div>
   );
 }
