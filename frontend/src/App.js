5748ead964c676915569d6aa9e53cff906bc5629
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-07-21 23:49:59 -0400

    Just another edit prior to building

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,10 @@ function App() {
       <h1>Hello React</h1>
       <h2>My Name is Universal Philosopher</h2>
       <p>This is my website</p>
+
+      <br />
+
+      <h3>Created by Von'Erik </h3>
     </div>
   );
 }
